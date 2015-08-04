111ae1053efa94534c26bf8c39087b3475bd609b
Haruo Fujiwara <haruo.fujiwara@icloud.com>
2015-08-04 11:41:41 +0900

    Added skeleton scenes.

---
+++ Assets/DynamicText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicText : MonoBehaviour {
+
+	TextMesh textmesh;
+
+	// Use this for initialization
+	void Start () {
+		textmesh = GetComponent<TextMesh>();
+		//textmesh.font;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
